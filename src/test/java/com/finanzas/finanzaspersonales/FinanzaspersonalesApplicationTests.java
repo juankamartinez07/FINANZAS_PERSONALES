c217e5c389cfd519a546c0f57ee7d5957bc42dd1
juankamartinez07 <juankamilogarcia1998@gmail.com>
2026-07-30 23:25:35 -0500

    Fix Railway MySQL datasource configuration

--- src/test/java/com/finanzas/finanzaspersonales/FinanzaspersonalesApplicationTests.java
+++ src/test/java/com/finanzas/finanzaspersonales/FinanzaspersonalesApplicationTests.java
@@ -3,7 +3,14 @@ package com.finanzas.finanzaspersonales;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:finanzaspersonales_test;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class FinanzaspersonalesApplicationTests {
 
 	@Test
